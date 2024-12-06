6deea893a62bc8f235098af4899bfbe5daa6143d
ShockwavePrime <sef0117@outlook.com>
2024-12-06 13:19:00 -0500

    fix: num_words, char_count functions

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = number_of_words(text)
+    print("Number of words:", num_words)
+    char_count = character_count(text)
+    print("Character count for each character is:\n"+ str(char_count))
 
-def number_of_words(file_contents):
-    words = file_contents.split()
-    word_count = len(words)
-    print("Number of words:", word_count)
 
-def character_count(file_contents):
-    output = {x : file_contents.count(x) for x in set(file_contents)}
-    print("Character count for each character is:\n"+ str(output))
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+        
 
+def number_of_words(text):
+    words = text.split()
+    return len(words)
+    
+
+def character_count(text):
+    lowered_text = text.lower()
+    return {x : lowered_text.count(x) for x in set(lowered_text)}
+    
 main()
+
 
